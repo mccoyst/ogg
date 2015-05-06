96678502bf3253bbc22bef66ba6b99b0aac20945
Steve McCoy <mccoyst@gmail.com>
2015-05-05 22:22:37 -0400

    Make the ErrBadCrc message more useful
     Slim chance of being a breaking change.

--- decode.go
+++ decode.go
@@ -8,6 +8,7 @@ import (
 	"errors"
 	"hash/crc32"
 	"io"
+	"strconv"
 )
 
 // A Decoder decodes an ogg stream page-by-page with its Decode method.
@@ -24,20 +25,29 @@ func NewDecoder(r io.Reader) *Decoder {
 // A Page represents a logical ogg page.
 type Page struct {
 	// Type is a bitmask of COP, BOS, and/or EOS.
-	Type    byte
+	Type byte
 	// Serial is the bitstream serial number.
-	Serial  uint32
+	Serial uint32
 	// Granule is the granule position, whose meaning is dependent on the encapsulated codec.
 	Granule int64
 	// Packet is the raw packet data.
 	// If Type & COP != 0, this is a continuation of the previous page's packet.
-	Packet  []byte
+	Packet []byte
 }
 
 // ErrBadSegs is the error used when trying to decode a page with a segment table size less than 1.
 var ErrBadSegs = errors.New("invalid segment table size")
+
 // ErrBadCrc is the error used when an ogg page's CRC field does not match the CRC calculated by the Decoder.
-var ErrBadCrc = errors.New("invalid crc in packet")
+type ErrBadCrc struct {
+	Found    uint32
+	Expected uint32
+}
+
+func (bc ErrBadCrc) Error() string {
+	return "invalid crc in packet: got " + strconv.FormatInt(int64(bc.Found), 16) +
+		", expected " + strconv.FormatInt(int64(bc.Found), 16)
+}
 
 var oggs = []byte{'O', 'g', 'g', 'S'}
 
@@ -111,7 +121,7 @@ func (d *Decoder) Decode() (Page, error) {
 	page[25] = 0
 	crc := crc32.Checksum(page, crcTable)
 	if crc != h.Crc {
-		return Page{}, ErrBadCrc
+		return Page{}, ErrBadCrc{h.Crc, crc}
 	}
 
 	return Page{h.HeaderType, h.Serial, h.Granule, packet}, nil
